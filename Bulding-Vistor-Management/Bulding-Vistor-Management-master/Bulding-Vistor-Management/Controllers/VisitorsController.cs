23feba93cb168baffd7dada2e8a6401e6b1f7517
sophephela <101864741+sophephela@users.noreply.github.com>
2023-09-03 15:13:38 +0200

    Add files via upload

---
+++ Bulding-Vistor-Management/Bulding-Vistor-Management-master/Bulding-Vistor-Management/Controllers/VisitorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bulding_Vistor_Management.Models;
+
+namespace Bulding_Vistor_Management.Controllers
+{
+    public class VisitorsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Visitors
+        public ActionResult Index()
+        {
+            return View(db.Visitors.ToList());
+        }
+
+        // GET: Visitors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visitor visitor = db.Visitors.Find(id);
+            if (visitor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visitor);
+        }
+
+        // GET: Visitors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Visitors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VisitorId,Fullnames,Address,Email,cell")] Visitor visitor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Visitors.Add(visitor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(visitor);
+        }
+
+        // GET: Visitors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visitor visitor = db.Visitors.Find(id);
+            if (visitor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visitor);
+        }
+
+        // POST: Visitors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VisitorId,Fullnames,Address,Email,cell")] Visitor visitor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(visitor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(visitor);
+        }
+
+        // GET: Visitors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visitor visitor = db.Visitors.Find(id);
+            if (visitor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visitor);
+        }
+
+        // POST: Visitors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Visitor visitor = db.Visitors.Find(id);
+            db.Visitors.Remove(visitor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Bulding-Vistor-Management/Bulding-Vistor-Management-master/Bulding-Vistor-Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bulding_Vistor_Management.MvcApplication" Language="C#" %>
